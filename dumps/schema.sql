7dfd8d47b8d6b78293f4f493524892bb87ab77cc
zefixed <gerasenkov1@mail.ru>
2024-12-02 17:42:05 +0300

    Update schema

--- dumps/schema.sql
+++ dumps/schema.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 17.2 (Debian 17.2-1.pgdg120+1)
--- Dumped by pg_dump version 17.2 (Debian 17.2-1.pgdg120+1)
+-- Dumped from database version 17.0 (Debian 17.0-1.pgdg120+1)
+-- Dumped by pg_dump version 17.0 (Debian 17.0-1.pgdg120+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,6 +20,20 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: additional; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.additional (
+    id integer,
+    field1 character varying(255),
+    field2 integer,
+    field3 date
+);
+
+
+ALTER TABLE public.additional OWNER TO postgres;
 
 --
 -- Name: alembic_version; Type: TABLE; Schema: public; Owner: postgres
@@ -310,3 +324,4 @@ ALTER TABLE ONLY public.transactions
 --
 -- PostgreSQL database dump complete
 --
+
